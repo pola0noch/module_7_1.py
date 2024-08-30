0737c078806dd07d915f90a7ef93cbee14cad26b
pola0noch <klayriddell@gmail.com>
2024-08-30 14:22:39 +0300

    init

---
+++ module_7_1.py
@@ -0,0 +1,54 @@
+# Домашнее задание по теме "Режимы открытия файлов"
+from pprint import pprint
+
+class Product:
+    def __init__(self, name:str, weight:float, category:str):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f"{self.name}, Вес: {self.weight}, Категория: {self.category}"
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        self.file = open(self.__file_name, "r")
+        return self.file.read()
+        self.file.close()
+
+    def add(self, *products):
+        for product in products:
+            self.file = open(self.__file_name, 'r')
+            product_str = str(product)
+            if product_str not in self.file.read():
+                self.file = open(self.__file_name, "a")
+                self.file.write(f"{product_str}\n")
+                self.file.close()
+            else:
+                print(f"Продукт {product.name} уже есть в магазине")
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
+
+
+
+
+
+
+
+
+
+
+
